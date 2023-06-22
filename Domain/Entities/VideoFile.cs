c21c8f56eef33e3d23e5e6746a080d7f5a1334f1
Shayan Mirzaei <shayan.mirzaei@divar.ir>
2023-06-22 19:08:25 +0330

    Fix entity ids

--- Domain/Entities/VideoFile.cs
+++ Domain/Entities/VideoFile.cs
@@ -1,9 +1,12 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Monitoring.Site.Domain.Entities;
 
 public class VideoFile
 {
 
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public string Path { get; set; }
     [DataType(DataType.Date)]
