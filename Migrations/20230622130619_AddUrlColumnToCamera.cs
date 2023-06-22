413772ddeb1d3ea922b76b6d7dc814af9d2a197b
Iman malekanzadeh <iman.m@cafebazaar.ir>
2023-06-22 19:39:09 +0430

    Add camera apis

---
+++ Migrations/20230622130619_AddUrlColumnToCamera.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MonitoringNetCore.Migrations
+{
+    public partial class AddUrlColumnToCamera : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                    name: "Url",
+                    table: "Camera",
+                    type: "TEXT",
+                    nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
