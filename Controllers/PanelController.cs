f069c4ffb7e963e32a8166b25d26f9840a846066
Pouya Payandeh <pouya.payandeh@gmail.com>
2023-06-22 14:04:47 +0330

    Add Panel Controller

---
+++ Controllers/PanelController.cs
@@ -0,0 +1,24 @@
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Mvc;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace MonitoringNetCore.Controllers;
+
+public class PanelController : Controller
+{
+    IAmazonS3 S3Client { get; set; }
+    private readonly IHostingEnvironment hostingEnvironment;
+    public PanelController(IHostingEnvironment environment,IAmazonS3 s3Client)
+    {
+        hostingEnvironment = environment;
+        this.S3Client = s3Client;
+    }
+
+    // GET
+    public async Task<IActionResult> Index()
+    {
+        ListObjectsResponse result = await S3Client.ListObjectsAsync("uploads");
+        return View(result.S3Objects);
+    }
+}
